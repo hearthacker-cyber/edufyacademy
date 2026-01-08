6bbb0dff7e518a9ec2a0f84891164c4565279b39
hearthacker-cyber <hearthacks2020@gmail.com>
2026-01-08 19:46:27 +0530

    get message

--- auth/google-login.php
+++ auth/google-login.php
@@ -2,9 +2,9 @@
 require_once '../../vendor/autoload.php';
 
 $client = new Google_Client();
-// $client->setClientId('902927455984-aq7hrkrojic09434p2pab84njhsula1s.apps.googleusercontent.com');
-// $client->setClientSecret('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');
-// $client->setRedirectUri('https://edufyacademy.com/app/auth/google-callback.php');
+$client->setClientId('902927455984-aq7hrkrojic09434p2pab84njhsula1s.apps.googleusercontent.com');
+$client->setClientSecret('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');
+$client->setRedirectUri('https://edufyacademy.com/app/auth/google-callback.php');
 $client->addScope('email');
 $client->addScope('profile');
 
