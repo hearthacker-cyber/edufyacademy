1b172f3e379bfaec736a82411c390cb5a2ca58ae
hearthacker-cyber <hearthacks2020@gmail.com>
2026-01-08 19:44:40 +0530

    get message

--- auth/google-callback.php
+++ auth/google-callback.php
@@ -32,7 +32,10 @@ if (session_status() === PHP_SESSION_NONE) {
 }
 
 $client = new Google_Client();
-
+$client->setClientId('902927455984-aq7hrkrojic09434p2pab84njhsula1s.apps.googleusercontent.com');
+$client->setClientSecret('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');
+$client->setRedirectUri('https://edufyacademy.com/app/auth/google-callback.php');
+$client->addScope(['email', 'profile']);
 
 if (isset($_GET['code'])) {
     try {
